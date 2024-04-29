7b0ec09b3dc78396b26271d055ab4d7cd5d42990
David <soydoft@gmail.com>
2024-04-29 17:54:30 -0400

    change stylesheet

--- tictactoe.js
+++ tictactoe.js
@@ -43,19 +43,20 @@ function game(index) {
     if (grid[index] === 0) {
          // change grid array to reflect new move
         placeX(index);
+
+        //computer randomly selects a tictactoe box number
+        //validate cpu turn
+        let random = 0;
+        do {
+            random = Math.floor(Math.random() * grid.length);
+        } while (grid[random] !== 0)
+        placeO(random);
+
     } else {
         displayResult("SPOT TAKEN!");
+        setTimeout(() => clearResult(), 1000);
     }
 
-    //computer randomly selects a tictactoe box number
-    //validate cpu turn
-    let random = 0;
-    let i = 0;
-    do {
-        random = Math.floor(Math.random() * grid.length);
-        i++;
-    } while (grid[random] !== 0 && i > 9)
-    placeO(random);
     
 }
 
@@ -65,6 +66,7 @@ function displayResult(status) {
     const resultPopup = document.querySelector("#result-popup");
     resultStatement.innerHTML = status;
     resultPopup.style.display = "flex";
+
 }
 
 //place X on board
@@ -74,6 +76,7 @@ function placeX(index) {
     square.innerHTML = "X";
     grid[index] = 1;
     checkWin(index);
+    checkTie();
 }
 
 //place O on board
@@ -82,219 +85,259 @@ function placeO(random) {
     console.log("choice" + conversion[random]);
     square.innerHTML = 0;
     grid[random] = -1;
-    checkWin(random);
+    checkLose(random);
 }
 
 //check if won game
 function checkWin(index) {
     switch (index) {
-        case 0:
-            let grid_0 = grid[0];
+        case 0: {
+            const grid0 = grid[0];
 
-            if (grid_0 === -1 || grid_0 === 1) {
+            if (grid0 === -1 || grid0 === 0) {
                 break;
-            } else if (grid_0 === grid[1] && grid_0 === grid[2]) {
+            } else if (grid0 === grid[1] && grid0 === grid[2]) {
                 win();
-            } else if (grid_0 === grid[3] && grid_0 === grid[6]) {
+            } else if (grid0 === grid[3] && grid0 === grid[6]) {
                 win();
-            } else if (grid_0 === grid[4] && grid_0 === grid[8]) {
+            } else if (grid0 === grid[4] && grid0 === grid[8]) {
                 win();
             }
             break;
-        case 1:
-            let grid_1 = grid[1];
-            if (grid_1 === -1 || grid_1 && grid_1 === 1) {
+        }
+        case 1: {
+            const grid1 = grid[1];
+            if (grid1 === -1 || grid1 && grid1 === 0) {
                 break;
-            } else if (grid_1 === grid[0] && grid_1 === grid[2]) {
+            } else if (grid1 === grid[0] && grid1 === grid[2]) {
                 win();
-            } else if (grid_1 === grid[4] && grid_1 === grid[7]) {
+            } else if (grid1 === grid[4] && grid1 === grid[7]) {
                 win();
             }
             break;
-        case 2:
-            if (grid[2] === -1 || grid[2] === 1) {
+        }
+        case 2: {
+            const grid2 = grid[2];
+            if (grid2 === -1 || grid2 === 0) {
                 break;
-            } else if (grid[2] === grid[1] && grid[2] === grid[0]) {
+            } else if (grid2 === grid[1] && grid2 === grid[0]) {
                 win();
-            } else if (grid[2] === grid[5] && grid[2] === grid[8]) {
+            } else if (grid2 === grid[5] && grid2 === grid[8]) {
                 win();
-            } else if (grid[2] === grid[4] && grid[2] === grid[6]) {
+            } else if (grid2 === grid[4] && grid2 === grid[6]) {
                 win();
             }
             break;
-        case 3:
-            if (grid[3] === -1 || grid[3] === 1) {
+        }
+        case 3: {
+            const grid3 = grid[3];
+            if (grid3 === -1 || grid3 === 0) {
                 break;
-            } else if (grid[3] === grid[4] && grid[3] === grid[5]) {
+            } else if (grid3 === grid[4] && grid3 === grid[5]) {
                 win();
-            } else if (grid[3] === grid[0] && grid[3] === grid[6]) {
+            } else if (grid3 === grid[0] && grid3 === grid[6]) {
                 win();
             }
             break;
-        case 4:
-            if (grid[4] === -1 || grid[4] === 1) {
+        }
+        case 4: {
+            const grid4 = grid[4];
+            if (grid4 === -1 || grid4 === 0) {
                 break;
-            } else if (grid[4] === grid[3] && grid[4] === grid[5]) {
+            } else if (grid4 === grid[3] && grid4 === grid[5]) {
                 win();
-            } else if (grid[4] === grid[1] && grid[4] === grid[7]) {
+            } else if (grid4 === grid[1] && grid4 === grid[7]) {
                 win();
-            } else if (grid[4] === grid[0] && grid[4] === grid[8]) {
+            } else if (grid4 === grid[0] && grid4 === grid[8]) {
                 win();
-            } else if (grid[4] === grid[2] && grid[4] === grid[6]) {
+            } else if (grid4 === grid[2] && grid4 === grid[6]) {
                 win();
             }  
             break;
-        case 5:
-            if (grid[5] === -1 || grid[5] === 1) {
+        }
+        case 5: {
+            const grid5 = grid[5];
+            if (grid5 === -1 || grid5 === 0) {
                 break;
-            } else if (grid[5] === grid[4] && grid[5] === grid[3]) {
+            } else if (grid5 === grid[4] && grid5 === grid[3]) {
                 win();
-            } else if (grid[5] === grid[8] && grid[5] === grid[2]) {
+            } else if (grid5 === grid[8] && grid5 === grid[2]) {
                     win();
             }
             break;
-        case 6:
-            if (grid[6] === -1 || grid[6] === 1) {
+        }
+        case 6: {
+            const grid6 = grid[6];
+            if (grid6 === -1 || grid6 === 0) {
                 break;
-            } else if (grid[6] === grid[7] && grid[6] === grid[8]) {
+            } else if (grid6 === grid[7] && grid6 === grid[8]) {
                 win();
-            } else if (grid[6] === grid[0] && grid[6] === grid[3]) {
+            } else if (grid6 === grid[0] && grid6 === grid[3]) {
                     win();
-            } else if (grid[6] === grid[4] && grid[6] === grid[2]) {
+            } else if (grid6 === grid[4] && grid6 === grid[2]) {
                     win();
             }
             break;
-        case 7:
-            if (grid[7] === -1 || grid[7] === 1) {
+        }
+        case 7: {
+            const grid7 = grid[7];
+            if (grid7 === -1 || grid7 === 0) {
                 break;
-            } else if (grid[7] === grid[6] && grid[7] === grid[7]) {
+            } else if (grid7 === grid[6] && grid7 === grid[8]) {
                 win();
-            } else if (grid[7] === grid[4] && grid[7] === grid[1]) {
+            } else if (grid7 === grid[4] && grid7 === grid[1]) {
                     win();
             }
             break;
-        case 8:
-            if (grid[8] === -1 || grid[8] === 1) {
+        }
+        case 8: {
+            const grid8 = grid[8];
+            if (grid8 === -1 || grid8 === 1) {
                 break;
-            } else if (grid[8] === grid[7] && grid[8] === grid[6]) {
+            } else if (grid8 === grid[7] && grid8 === grid[6]) {
                 win();
-            } else if (grid[8] === grid[5] && grid[8] === grid[2]) {
+            } else if (grid8 === grid[5] && grid8 === grid[2]) {
                     win();
-            } else if (grid[8] === grid[4] && grid[8]=== grid[1]) {
+            } else if (grid8 === grid[4] && grid8=== grid[1]) {
                     win();
             }
             break;
-        
+        }
     }
 }
 
 //check if cpu won
-function checkCPUWin(random) {
-        switch (random) {
-            case 0:
-                let grid_0 = grid[0];
-    
-                if (grid_0 === -1 || grid_0 === 1) {
-                    break;
-                } else if (grid_0 === grid[1] && grid_0 === grid[2]) {
-                    win();
-                } else if (grid_0 === grid[3] && grid_0 === grid[6]) {
-                    win();
-                } else if (grid_0 === grid[4] && grid_0 === grid[8]) {
-                    win();
-                }
+function checkLose(random) {
+    switch (random) {
+        case 0: {
+            const grid0 = grid[0];
+
+            if (grid0 === 1 || grid0 === 0) {
                 break;
-            case 1:
-                let grid_1 = grid[1];
-                if (grid_1 === -1 || grid_1 && grid_1 === 1) {
-                    break;
-                } else if (grid_1 === grid[0] && grid_1 === grid[2]) {
-                    win();
-                } else if (grid_1 === grid[4] && grid_1 === grid[7]) {
-                    win();
-                }
+            } else if (grid0 === grid[1] && grid0 === grid[2]) {
+                lose();
+            } else if (grid0 === grid[3] && grid0 === grid[6]) {
+                lose();
+            } else if (grid0 === grid[4] && grid0 === grid[8]) {
+                lose();
+            }
+            break;
+        }
+        case 1: {
+            const grid1 = grid[1];
+            if (grid1 === -1 || grid1 && grid1 === 0) {
                 break;
-            case 2:
-                if (grid[2] === -1 || grid[2] === 1) {
-                    break;
-                } else if (grid[2] === grid[1] && grid[2] === grid[0]) {
-                    win();
-                } else if (grid[2] === grid[5] && grid[2] === grid[8]) {
-                    win();
-                } else if (grid[2] === grid[4] && grid[2] === grid[6]) {
-                    win();
-                }
+            } else if (grid1 === grid[0] && grid1 === grid[2]) {
+                lose();
+            } else if (grid1 === grid[4] && grid1 === grid[7]) {
+                lose();
+            }
+            break;
+        }
+        case 2: {
+            const grid2 = grid[2];
+            if (grid2 === 1 || grid2 === 0) {
                 break;
-            case 3:
-                if (grid[3] === -1 || grid[3] === 1) {
-                    break;
-                } else if (grid[3] === grid[4] && grid[3] === grid[5]) {
-                    win();
-                } else if (grid[3] === grid[0] && grid[3] === grid[6]) {
-                    win();
-                }
+            } else if (grid2 === grid[1] && grid2 === grid[0]) {
+                lose();
+            } else if (grid2 === grid[5] && grid2 === grid[8]) {
+                lose();
+            } else if (grid2 === grid[4] && grid2 === grid[6]) {
+                lose();
+            }
+            break;
+        }
+        case 3: {
+            const grid3 = grid[3];
+            if (grid3 === 1 || grid3 === 0) {
                 break;
-            case 4:
-                if (grid[4] === -1 || grid[4] === 1) {
-                    break;
-                } else if (grid[4] === grid[3] && grid[4] === grid[5]) {
-                    win();
-                } else if (grid[4] === grid[1] && grid[4] === grid[7]) {
-                    win();
-                } else if (grid[4] === grid[0] && grid[4] === grid[8]) {
-                    win();
-                } else if (grid[4] === grid[2] && grid[4] === grid[6]) {
-                    win();
-                }  
+            } else if (grid3 === grid[4] && grid3 === grid[5]) {
+                lose();
+            } else if (grid3 === grid[0] && grid3 === grid[6]) {
+                lose();
+            }
+            break;
+        }
+        case 4: {
+            const grid4 = grid[4];
+            if (grid4 === 1 || grid4 === 0) {
                 break;
-            case 5:
-                if (grid[5] === -1 || grid[5] === 1) {
-                    break;
-                } else if (grid[5] === grid[4] && grid[5] === grid[3]) {
-                    win();
-                } else if (grid[5] === grid[8] && grid[5] === grid[2]) {
-                        win();
-                }
+            } else if (grid4 === grid[3] && grid4 === grid[5]) {
+                lose();
+            } else if (grid4 === grid[1] && grid4 === grid[7]) {
+                lose();
+            } else if (grid4 === grid[0] && grid4 === grid[8]) {
+                lose();
+            } else if (grid4 === grid[2] && grid4 === grid[6]) {
+                lose();
+            }  
+            break;
+        }
+        case 5: {
+            const grid5 = grid[5];
+            if (grid5 === 1 || grid5 === 0) {
                 break;
-            case 6:
-                if (grid[6] === -1 || grid[6] === 1) {
-                    break;
-                } else if (grid[6] === grid[7] && grid[6] === grid[8]) {
-                    win();
-                } else if (grid[6] === grid[0] && grid[6] === grid[3]) {
-                        win();
-                } else if (grid[6] === grid[4] && grid[6] === grid[2]) {
-                        win();
-                }
+            } else if (grid5 === grid[4] && grid5 === grid[3]) {
+                lose();
+            } else if (grid5 === grid[8] && grid5 === grid[2]) {
+                lose();
+            }
+            break;
+        }
+        case 6: {
+            const grid6 = grid[6];
+            if (grid6 === 1 || grid6 === 0) {
                 break;
-            case 7:
-                if (grid[7] === -1 || grid[7] === 1) {
-                    break;
-                } else if (grid[7] === grid[6] && grid[7] === grid[7]) {
-                    win();
-                } else if (grid[7] === grid[4] && grid[7] === grid[1]) {
-                        win();
-                }
+            } else if (grid6 === grid[7] && grid6 === grid[8]) {
+                lose();
+            } else if (grid6 === grid[0] && grid6 === grid[3]) {
+                lose();
+            } else if (grid6 === grid[4] && grid6 === grid[2]) {
+                lose();
+            }
+            break;
+        }
+        case 7: {
+            const grid7 = grid[7];
+            if (grid7 === 1 || grid7 === 0) {
                 break;
-            case 8:
-                if (grid[8] === -1 || grid[8] === 1) {
-                    break;
-                } else if (grid[8] === grid[7] && grid[8] === grid[6]) {
-                    win();
-                } else if (grid[8] === grid[5] && grid[8] === grid[2]) {
-                        win();
-                } else if (grid[8] === grid[4] && grid[8] === grid[1]) {
-                        win();
-                }
+            } else if (grid7 === grid[6] && grid7 === grid[8]) {
+                lose();
+            } else if (grid7 === grid[4] && grid7 === grid[1]) {
+                lose();
+            }
+            break;
+        }
+        case 8: {
+            const grid8 = grid[8];
+            if (grid8 === 1 || grid8 === 1) {
                 break;
-            
+            } else if (grid8 === grid[7] && grid8 === grid[6]) {
+                lose();
+            } else if (grid8 === grid[5] && grid8 === grid[2]) {
+                lose();
+            } else if (grid8 === grid[4] && grid8=== grid[1]) {
+                lose();
+            }
+            break;
         }
     }
+}
+
+function checkTie() {
+    if (!grid.includes(0)) {
+        displayResult("YOU TIED!!");
+        setTimeout(() => removePopup, 1000);
+        setTimeout(() => restart(), 2000);
+    }
+    
+}
 
 function win() {
     //Display result and clear after 1 sec
     displayResult("YOU WIN!");
-    setTimeout(() => clearResult(), 1000)
+    setTimeout(() => clearResult(), 1000);
+    setTimeout(() => restart(), 2000);
 
     //increment user score
     userScore++;
@@ -307,25 +350,29 @@ function lose() {
     //Display result and clear after 1 sec
     displayResult("YOU LOSE!");
     setTimeout(() => clearResult(), 1000);
+    setTimeout(() => restart(), 2000);
 
+    //increment opponent score
     oppScore++;
     oppScoreDisplay.innerHTML = oppScore;
     scoreStatus();
 }
 
 function clearResult() {
-    //clear cpu choice background color
-    square1x1.innerHTML = 0;
-    square1x2.innerHTML = 0;
-    square1x3.innerHTML = 0;
-    square2x1.innerHTML = 0;
-    square2x2.innerHTML = 0;
-    square2x3.innerHTML = 0;
-    square3x1.innerHTML = 0;
-    square3x2.innerHTML = 0;
-    square3x3.innerHTML = 0;
-    grid.fill(0);
-    //clear popup
     const resultPopup = document.querySelector("#result-popup");
     resultPopup.style.display = "none";
+}
+
+function restart() {
+    //clear cpu choice background color
+    square1x1.innerHTML = "";
+    square1x2.innerHTML = "";
+    square1x3.innerHTML = "";
+    square2x1.innerHTML = "";
+    square2x2.innerHTML = "";
+    square2x3.innerHTML = "";
+    square3x1.innerHTML = "";
+    square3x2.innerHTML = "";
+    square3x3.innerHTML = "";
+    grid.fill(0);
 }
